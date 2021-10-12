b65e45878517cd727ebfc4c0d5518308d9d4f79a
Abhijeet <sonawaneabhijeet273@gmail.com>
2021-10-12 16:19:09 +0530

    Updated admin.py

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Exam
 
 # Register your models here.
-@admin.register(Exam)
-class ExamAdmin(admin.ModelAdmin):
-    list_display = ['id', 'Question', 'Option1', 'Option2', 'Option3', 'Option4', 'Corrans'] 
